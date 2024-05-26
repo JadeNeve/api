d23f7809ea6d5c72c8cf05b37ddf3d11b4ec5028
Jade-Neve Blows <118060796+JadeNeve@users.noreply.github.com>
2024-05-26 22:15:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
-const serve = jsonServer.create();
+const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-serve.use(middlewares);
-serve.use(router);
+server.use(middlewares);
+server.use(router);
 
-serve.listen(port);
+server.listen(port);
